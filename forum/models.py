b521d958540f24865c36f395349a2176001e6611
ramsuthar305 <ramsuthar305@gmail.com>
2020-06-20 21:12:49 +0530

    forum complete backend

--- forum/models.py
+++ forum/models.py
@@ -1,119 +1,208 @@
 import hashlib
 from app import *
 from flask import session
-import os 
-#from pyresparser import ResumeParser
+import os
+# from pyresparser import ResumeParser
 from bson import ObjectId
 
 
-
 class Forum_model:
-	def __init__(self):
-		self.mongo = mongo.db 
-	
-	def new_post(self,data):
-		try:
-			post = self.mongo.forum.insert_one(data)
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
-		
-	def new_answer(self,answer,post_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id)},{"$push":{"answers":answer}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def __init__(self):
+        self.mongo = mongo.db
 
-	def new_report(self,report,post_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id)},{"$push":{"reports":report}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_post(self, data):
+        try:
+            post = self.mongo.forum.insert_one(data)
+            post = self.mongo.forum.find_one(
+                {"_id": ObjectId(post.inserted_id)})
+            print(post)
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
 
-	def new_upvote(self,upvote_data,post_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id)},{"$push":{"upvotes":upvote_data}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_answer(self, answer, post_id):
+        try:
+            status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                "$push": {"answers": answer}})
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
 
-	def new_downvote(self,downvote_data,post_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id)},{"$push":{"downvotes":downvote_data}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_report(self, report, post_id):
+        try:
+            status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                "$push": {"reports": report}})
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
 
-	def new_comment(self,comment_data,post_id,answer_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id),"answers":{"$elemMatch":{"_id":ObjectId(answer_id)}}},{"$push":{"answer.$.comments":comment_data}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_upvote(self, upvote_data, post_id):
+        try:
+            upvote_exists = list(self.mongo.forum.find(
+                {"upvotes": {"$elemMatch": {"uid": upvote_data['uid']}}}))
+            if len(upvote_exists) == 0:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                    "$push": {"upvotes": upvote_data}})
+            else:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                    "$pull": {"upvotes": {"uid": upvote_data['uid']}}})
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
 
-	def new_report(self,report_data,post_id,answer_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id),"answers":{"$elemMatch":{"_id":ObjectId(answer_id)}}},{"$push":{"answer.$.reports":report_data}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_downvote(self, downvote_data, post_id):
+        try:
+            downvote_exists = list(self.mongo.forum.find(
+                {"downvotes": {"$elemMatch": {"uid": downvote_data['uid']}}}))
+            if len(downvote_exists) == 0:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                    "$push": {"downvotes": downvote_data}})
+            else:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                    "$pull": {"downvotes": {"uid": downvote_data['uid']}}})
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
 
-	def new_answer_like(self,answer_like_data,post_id,answer_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id),"answers":{"$elemMatch":{"_id":ObjectId(answer_id)}}},{"$push":{"answer.$.answer_likes":answer_like_data}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_comment(self, comment_data, post_id, answer_id):
+        try:
+            status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                "_id": ObjectId(answer_id)}}}, {"$push": {"answers.$.comments": comment_data}})
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
 
-	def new_answer_dislike(self,answer_dislike_data,post_id,answer_id):
-		try:
-			status = self.mongo.forum.update_one({"_id":ObjectId(post_id),"answers":{"$elemMatch":{"_id":ObjectId(answer_id)}}},{"$push":{"answer.$.answer_dislikes":answer_dislike_data}})
-			post = self.mongo.forum.find_one({"_id":ObjectId(post_id)})
-			if post != None:
-				return post
-			else:
-				return False
-		except Exception as error:
-			print('In exception :',error)
-			return error
+    def new_answer_report(self, report_data, post_id, answer_id):
+        try:
+            status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                "_id": ObjectId(answer_id)}}}, {"$push": {"answers.$.reports": report_data}})
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
+
+    def new_answer_upvote(self, answer_upvote_data, post_id, answer_id):
+        try:
+            downvote_exists = list(self.mongo.forum.find(
+                {"answers.downvotes": {"$elemMatch": {"uid": answer_upvote_data['uid']}}}))
+            print("\n\n this is sttus: ", downvote_exists)
+            if len(downvote_exists) != 0:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                                                     "_id": ObjectId(answer_id)}}}, {"$pull": {"answers.$.downvotes":  {"uid": answer_upvote_data['uid']}}})
+
+            upvote_exists = list(self.mongo.forum.find(
+                {"answers.upvotes": {"$elemMatch": {"uid": answer_upvote_data['uid']}}}))
+            print("\n\n this is sttus: ", upvote_exists)
+            if len(upvote_exists) == 0:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                                                     "_id": ObjectId(answer_id)}}}, {"$push": {"answers.$.upvotes": answer_upvote_data}})
+            else:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                                                     "_id": ObjectId(answer_id)}}}, {"$pull": {"answers.$.upvotes":  {"uid": answer_upvote_data['uid']}}})
+
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
+
+    def new_answer_downvote(self, answer_downvote_data, post_id, answer_id):
+        try:
+            upvote_exists = list(self.mongo.forum.find(
+                {"answers.upvotes": {"$elemMatch": {"uid": answer_downvote_data['uid']}}}))
+            print("\n\n this is sttus: ", upvote_exists)
+            if len(upvote_exists) != 0:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                                                     "_id": ObjectId(answer_id)}}}, {"$pull": {"answers.$.upvotes":  {"uid": answer_downvote_data['uid']}}})
+            downvote_exists = list(self.mongo.forum.find(
+                {"answers.downvotes": {"$elemMatch": {"uid": answer_downvote_data['uid']}}}))
+            print("\n\n this is sttus: ", downvote_exists)
+            if len(downvote_exists) == 0:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                                                     "_id": ObjectId(answer_id)}}}, {"$push": {"answers.$.downvotes": answer_downvote_data}})
+            else:
+                status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {"_id": ObjectId(
+                    answer_id)}}}, {"$pull": {"answers.$.downvotes":  {"uid": answer_downvote_data['uid']}}})
+
+            post = self.mongo.forum.find_one({"_id": ObjectId(post_id)})
+            if post != None:
+                return post
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
+
+    def delete_post(self, post_id):
+        try:
+            status = self.mongo.forum.remove({"_id": ObjectId(post_id)})
+            print("\nthis is delete status: ", status['n'])
+            if status['n'] != 0:
+                return True
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
+
+    def delete_answer(self, post_id, answer_id):
+        try:
+            status = self.mongo.forum.update_one({"_id": ObjectId(post_id)}, {
+                                                 "$pull": {"answers": {"_id": ObjectId(answer_id)}}})
+            print("\nthis is delete status: ", status.modified_count)
+            if status.modified_count != 0:
+                return True
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
+
+    def delete_comment(self, post_id, answer_id, comment_id):
+        try:
+            status = status = self.mongo.forum.update_one({"_id": ObjectId(post_id), "answers": {"$elemMatch": {
+                                                          "_id": ObjectId(answer_id)}}}, {"$pull": {"answers.$.comments":  {"_id": ObjectId(comment_id)}}})
+            print("\nthis is delete status: ", status.modified_count)
+            if status.modified_count != 0:
+                return True
+            else:
+                return False
+        except Exception as error:
+            print('In exception :', error)
+            return error
--- forum/views.py
+++ forum/views.py
@@ -2,36 +2,205 @@ from flask import Blueprint, render_template, request, redirect, session, jsonif
 from functools import wraps
 from flask import session
 import hashlib
+from bson.json_util import dumps
 import json
 from datetime import datetime
-#custom imports
+# custom imports
 from .models import Forum_model
+from bson import ObjectId
+
 
 forum_obj = Forum_model()
 
-forum = Blueprint("forum", __name__, template_folder='../template', static_folder='../static',static_url_path='../static')
-
+forum = Blueprint("forum", __name__, template_folder='../template',
+                  static_folder='../static', static_url_path='../static')
 
 
 @forum.errorhandler(404)
 def page_not_found(error):
-	return render_template('forum/404.html'), 404
+    return render_template('forum/404.html'), 404
 
-@forum.route('/new_post',methods=['GET','POST'])
+
+@forum.route('/new_post', methods=['GET', 'POST'])
 def new_post():
-	if request.method=="POST":
-		print("inside the post")
-		title=request.form.get("title")
-		category=request.form.get("category")
-		description=request.form.get("description")
-		data={}
-		data['title']=title
-		data['category']=category
-		data['description']=description
-		print(data)
-		return jsonify(data)
+    if request.method == "POST":
+        print("inside the post")
+        title = request.form.get("title")
+        category = request.form.get("category")
+        description = request.form.get("description")
+        created_on = datetime.now()
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['title'] = title
+        data['category'] = category
+        data['description'] = description
+        data['created_on'] = created_on
+        data['uid'] = uid
+        data['answers'] = []
+        data['upvotes'] = []
+        data['downvotes'] = []
+        data['reports'] = []
+        result = forum_obj.new_post(data)
+        print("this is result: ", result)
+        return dumps(result)
 
 
+@forum.route('/new_answer', methods=['GET', 'POST'])
+def new_answer():
+    if request.method == "POST":
+        print("inside the post")
+        answer = request.form.get("answer")
+        created_on = datetime.now()
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['answer'] = answer
+        data['_id'] = ObjectId()
+        data['created_on'] = created_on
+        data['uid'] = uid
+        data['comments'] = []
+        data['upvotes'] = []
+        data['downvotes'] = []
+        data['reports'] = []
+        result = forum_obj.new_answer(data, "5eee171819f4780a00e54d0d")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_upvote', methods=['GET', 'POST'])
+def new_upvote():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['uid'] = uid
+        result = forum_obj.new_upvote(data, "5eee171819f4780a00e54d0d")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_downvote', methods=['GET', 'POST'])
+def new_downvote():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['uid'] = uid
+        result = forum_obj.new_downvote(data, "5eee171819f4780a00e54d0d")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_report', methods=['GET', 'POST'])
+def new_report():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        report = request.form.get("report")
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['_id'] = ObjectId()
+        data['uid'] = uid
+        data['report'] = report
+        result = forum_obj.new_report(data, "5eee171819f4780a00e54d0d")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_comment', methods=['GET', 'POST'])
+def new_comment():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        comment = request.form.get("comment")
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['_id'] = ObjectId()
+        data['uid'] = uid
+        data['comment'] = comment
+        result = forum_obj.new_comment(
+            data, "5eee171819f4780a00e54d0d", "5eee1c5b7e6b169efa66a58f")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_answer_report', methods=['GET', 'POST'])
+def new_answer_report():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        report = request.form.get("report")
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['_id'] = ObjectId()
+        data['uid'] = uid
+        data['report'] = report
+        result = forum_obj.new_answer_report(
+            data, "5eee171819f4780a00e54d0d", "5eee1c5b7e6b169efa66a58f")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_answer_upvote', methods=['GET', 'POST'])
+def new_answer_upvote():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['uid'] = uid
+        result = forum_obj.new_answer_upvote(
+            data, "5eee171819f4780a00e54d0d", "5eee1c5b7e6b169efa66a58f")
+        print("this is result: ", result)
+        return dumps(result)
+
+
+@forum.route('/new_answer_downvote', methods=['GET', 'POST'])
+def new_answer_downvote():
+    if request.method == "POST":
+        print("inside the post")
+        created_on = datetime.now()
+        uid = "ramsuthar305@gmail.com"
+        data = {}
+        data['created_on'] = created_on
+        data['uid'] = uid
+        result = forum_obj.new_answer_downvote(
+            data, "5eee171819f4780a00e54d0d", "5eee1c5b7e6b169efa66a58f")
+        print("this is result: ", result)
+        return dumps(result)
+
+@forum.route('/delete_post', methods=["GET", "POST"])
+def delete_post():
+    post_id = "5eee2e3ecfd6bc8710d6df93"
+    result = forum_obj.delete_post(post_id)
+    print("this is result: ", result)
+    return dumps(result)
+
+
+@forum.route('/delete_answer', methods=["GET", "POST"])
+def delete_answer():
+    post_id = "5eee171819f4780a00e54d0d"
+    answer_id = "5eee1c5b7e6b169efa66a58f"
+    result = forum_obj.delete_answer(post_id, answer_id)
+    print("this is result: ", result)
+    return dumps(result)
+
+
+@forum.route('/delete_comment', methods=["GET", "POST"])
+def delete_comment():
+    post_id = "5eee171819f4780a00e54d0d"
+    answer_id = "5eee1c5b7e6b169efa66a58f"
+    comment_id = "5eee2c1672ca19fbc3fdba0d"
+    result = forum_obj.delete_comment(post_id, answer_id, comment_id)
+    print("this is result: ", result)
+    return dumps(result)
 
 # @forum.route('/')
 # def index():
